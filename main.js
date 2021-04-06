fc3307c9eb7d9372b3817b13e1cad961a393799f
TejuDevz <75902941+TejuDevz@users.noreply.github.com>
2021-04-06 19:18:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+status = "";
+video = "";
+
+function setup() {
+  canvas = createCanvas(480, 380);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+}
+
+function draw() {
+  image(video, 0, 0, 480, 380);
+}
+
+function start() {
+  objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+  document.getElementById("status").innerHTML = "status: Object detecting";
+}
+
+function modelLoaded() {
+  console.log("model Loaded!");
+  status = true;
+}
